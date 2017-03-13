4e06d408e506c2c91d194bf049fd04864f7c1760
xiongzhenggang <ab1290014555@qq.com>
2017-03-13 19:11:02 +0800

    web

---
+++ web相关/js部分.js
@@ -0,0 +1,108 @@
+
+Dom 操作表
+<script type="text/javascript">
+
+        window.onload=function(){
+            var tb1=document.getElementById("table1");
+            tb1.border="5px";
+            tb1.cellPadding="5px";
+            var img=document.getElementById("img1");
+            img.vspace="10";
+            //为每一个属性添加一个表格行
+            var row1=tb1.insertRow(-1);
+            //创建连个表格的单元格
+            var cel1=row1.insertCell(0);
+            var cel2=row1.insertCell(1);
+            //创建文本内容
+            var textArr1=document.createTextNode("src");
+            var textArr2=document.createTextNode("img.src");
+            cel1.appendChild(textArr1);
+            cel2.appendChild(textArr2);
+        }
+    </script>
+</head>
+<body>
+<p><img src="../images/01.jpg" class="testImage" alt="test image" id="img1"></p>
+<table id="table1" >
+    <tr><th>Attry</th><th>value</th></tr>
+</table>
+
+二、关于h5的本地存储
+1）localStorage的存储容量比cookie更大；
+
+（2）cookie作为http规范的一部分，它的主要作用是与服务器进行交互，使http保持连接状态。也就是你每次请求一个新的页面的时候，cookie都会被发送过去，这样无形中就浪费了宽带。
+
+（3)cookie保存是能指定可以访问该cookie的范围；localStorage的访问范围就是当前整个网站，不存在访问范围这个概念。且，两者都不支持跨域调用。
+
+html5中的Web Storage包括了两种存储方式： sessionStorage和localStorage；
+
+sessionStorage用于本地存储一个会话的数据，当会话结束时，存储的数据也会自动销毁（即当页面关闭的同时也销毁数据），因此，sessionStorage不是一个持久化的本地存储，仅仅是会话级别的存储。
+
+localStorage用于持久化的本地存储，除非手动删除数据，否则会一直保存。
+
+获取事件的通用写法
+function catchEvent(eventObj,event,eventHandler){
+    if(eventObj.addEventListener){
+        eventObj.addEventListener(event,eventHandler,false);
+    }else if(eventObj.attachEvent){
+        event="on"+ event;
+        eventHandler.attachEvent(event,eventHandler);
+    }
+}
+
+ajax技术跨浏览器：
+var xmlHttpobj;
+function getXmlHttp(){
+     var xmlHttp=null;
+     if(window.XMLHttpRequest) {
+         xmlHttp = new XMLHttpRequest()
+     }
+         else{
+             try{
+                 XMLHttp=new ActiveXObject("Msxm12.XMLHTTP");
+             }catch (e){
+                 try{
+                     XMLHttp=new ActiveXObject("Microsoft.XMLHTTP");
+                 }catch (e){
+                     return xmlHttp;
+                 }
+             }
+         }
+     return xmlHttp;
+ }
+/* open方法语法open(methon,url[,async,username,password])
+ 可选参数async表示异步默认true，其他表示指定服务用户名和密码
+ setRequestHeather（labbel，value）该方法为请求的头添加标记
+ sed（content）xmlHttp核心方法，发送请求同时附上相应的数据
+ getAllResponseHeader():它将以字符串的形式返回所有的http应答的头，
+ 其中包含激活超时值，内容类型服务相关信息和日期信息
+ getResponseHeader（label）：他将返回应答头指定的信息
+ abort（）：用来取消当前的请求
+*/
+Xmlhttp除了以上的6中方法还有6中属性：
+1、	onreadystatechange：用来保存当前请求ready状态改变时调用的函数
+2、	readyState：有五个可选值，0表示请求未初始化，1表示请求open阶段，2表示请求已发送，3表示正在接受应答，4表示应答已接受。
+3、	ResPonseText：文本格式信息
+4、	ResponseXML:xml格式的应答信息，可以将其视为有效的xml信息处理
+5、	Status：返回请求状态404,200.。
+6、	statusText：以文字表示请求状态
+
+下面对xmlHttp使用
+function populateList(){
+    //作用：可把字符串作为URI 组件进行编码。
+    var state=encodeURIComponent(document.getElementById("stateList").value);
+    var url='chl4-02.php?state='+state;
+    if(!xmlHttpobj){
+        xmlHttpobj=getXmlHttp();
+    }
+    if(!xmlHttpobj) return;
+    xmlHttpobj.open('GET',url,true);
+    xmlHttpobj.onreadystatechange=getCities;
+    xmlHttpobj.send(null);
+}
+/*escape()除了 ASCII 字母、数字和特定的符号外，对传进来的字符串全部进行转义编码，
+因此如果想对URL编码，最好不要使用此方法。
+而encodeURI() 用于编码整个URI,因为URI中的合法字符都不会被编码转换。
+encodeURIComponent方法在编码单个URIComponent（指请求参 数）应当是最常用的，
+它可以讲参数中的中文、特殊字符进行转义，而不会影响整个URL。*/
+
---
+++ web相关/使用ztee后台数据展示.js
@@ -0,0 +1,94 @@
+首先引入jQueryztree的源码及相关配置文件
+<script  src="js/jQuery/jquery-2.1.3.min.js"  type="text/javascript"></script> 
+<script src="js/ztree/jquery.ztree.core.min.js" type="text/javascript"></script>
+<script src="js/ztree/jquery.ztree.excheck.min.js" type="text/javascript"></script>
+<link href="js/ztree/zTreeStyle.css"  type="text/css" rel="stylesheet">
+
+<script type="text/javascript">
+/**
+ * 页面初始化
+ */
+$(document).ready(function(){
+  onLoadZTree();
+});
+/**
+ * 加载树形结构数据
+ */
+ var url="${ctx}/treeList.do";
+function onLoadZTree(){
+  var treeNodes;
+  $.ajax({
+    async:false,//是否异步
+    cache:false,//是否使用缓存
+    type:'POST',//请求方式：post
+    dataType:'json',//数据传输格式：json
+    url:url,//请求的action路径
+    error:function(){
+      //请求失败处理函数
+      alert('亲，请求失败！');
+    },
+    success:function(data){
+      //console.log(data);
+      //请求成功后处理函数
+      treeNodes = data;//把后台封装好的简单Json格式赋给treeNodes
+    }
+  });
+  var zTree;
+  var setting = {
+    view: {
+      dblClickExpand: false,//双击节点时，是否自动展开父节点的标识
+      showLine: true,//是否显示节点之间的连线
+      fontCss:{'color':'black','font-weight':'bold'},//字体样式函数
+      selectedMulti: false //设置是否允许同时选中多个节点
+    },
+    check:{
+      //chkboxType: { "Y": "ps", "N": "ps" },
+      chkStyle: "checkbox",//复选框类型
+      enable: true //每个节点上是否显示 CheckBox 
+    },
+    data: {
+      simpleData: {//简单数据模式
+        enable:true,
+        idKey: "id",
+        pIdKey: "pId",
+        rootPId: ""
+      },
+      key{
+    	  checked: "id",
+    	  url:"url"
+      }
+    },
+    callback: {
+      beforeClick: function(treeId, treeNode) {
+        zTree = $.fn.zTree.getZTreeObj("user_tree");
+        if (treeNode.isParent) {
+          zTree.expandNode(treeNode);//如果是父节点，则展开该节点
+        }else{
+          zTree.checkNode(treeNode, !treeNode.checked, true, true);//单击勾选，再次单击取消勾选
+        }
+      }
+    }
+  };
+  var t = $("#user_tree");
+  t = $.fn.zTree.init(t,setting,treeNodes);
+}
+</script>
+     <style type="text/css">
+     a{
+     font-size: 20px;
+     }
+     </style>
+     <script type="text/javascript">
+     $(function(){
+         $("#reg01").click(function(){
+            $.get("showUser.do",function(data){
+            	  window.location.href = "showUser.do";  
+            });
+     });
+     });
+     </script>
+
+
+<div class="zTreeDemoBackgroundleft">
+    <ul id="user_tree" class="ztree" style="border: 1px solid #617775;overflow-y: scroll;height: 500px;"></ul>
+  </div> 
